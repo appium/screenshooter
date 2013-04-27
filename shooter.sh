f465292053327cd06c552ec8f37a9286225ac34c
Eric Plaster <eric.plaster@mutualmobile.com>
2013-04-26 23:08:04 -0500

    Updated shell script for maven build.

--- shooter.sh
+++ shooter.sh
@@ -2,7 +2,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 JAR_CLASSPATH="$ANDROID_HOME/tools/lib/ddmlib.jar"
 JAR_CLASSPATH="$JAR_CLASSPATH:$ANDROID_HOME/tools/lib/x86_64/swt.jar"
-JAR_CLASSPATH="$JAR_CLASSPATH:$DIR/bin/ScreenShooter.jar"
+JAR_CLASSPATH="$JAR_CLASSPATH:$DIR/target/ScreenShooter-1.0.0-SNAPSHOT.jar"
 export JAR_CLASSPATH
 
-java -classpath $JAR_CLASSPATH io.appium.android.screenshooter.ScreenShooter
+echo $JAR_CLASSPATH
+
+java -classpath $JAR_CLASSPATH io.appium.android.screenshooter.ScreenShooter $1 $2
